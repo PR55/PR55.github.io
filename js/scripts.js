8f875e77e79a972c5440ba7e13f206d99d871052
Kyle Flores <floreskyle93@yahoo.com>
2024-09-18 16:34:56 -0400

    Updated and added failsafes if project sites go down

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,24 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+AbortSignal.timeout ??= async function timeout(ms) {
+    const ctrl = new AbortController()
+    setTimeout(() => ctrl.abort(), ms)
+    return ctrl.signal
+}
+
+let fetchCapstone = async (s = '') => {
+    console.log('b')
+    try{
+        let c = await fetch(s, {
+            signal:AbortSignal.timeout(2000)
+        })
+        if(c.ok) return 1
+        else return 0
+    }catch(e){
+        console.log(e)
+        return -1
+    }
+
+
+}
